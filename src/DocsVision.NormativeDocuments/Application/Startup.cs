427b1dfddc4e08208a637990da8962bc549cd34b
Dmitry Kalinov <kalinov@mercurydevelopment.com>
2017-11-24 17:27:38 +0400

    Add Authentication namespaces in Startup

--- src/DocsVision.NormativeDocuments/Application/Startup.cs
+++ src/DocsVision.NormativeDocuments/Application/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
